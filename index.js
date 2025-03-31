3264630cfa18987a97dfab8332cfe18662e5148e
souleyouatt <160172658+souleyouatt@users.noreply.github.com>
2025-03-31 19:31:53 +0000

    Update index.js

--- index.js
+++ index.js
@@ -94,3 +94,11 @@ window.addEventListener("load", function () {
         // Start auto-sliding when page loads
         startAutoSlide();
     });
+document.getElementById("mobile-menu").addEventListener("click", function () {
+    let navMenu = document.getElementById("nav-menu");
+    if (navMenu.style.display === "block") {
+        navMenu.style.display = "none";
+    } else {
+        navMenu.style.display = "block";
+    }
+});
